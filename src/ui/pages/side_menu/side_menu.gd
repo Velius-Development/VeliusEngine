4f45d66732ca4aa305e8df24d1c3790c344bd1b7
SpyrexDE <malikfk100@gmail.com>
2021-10-14 13:02:38 +0200

    Added close() to side_menu

--- src/ui/pages/side_menu/side_menu.gd
+++ src/ui/pages/side_menu/side_menu.gd
@@ -3,6 +3,7 @@ extends Control
 func _ready():
 	self.rect_global_position = Vector2( - 1440, 0)
 	$HBoxContainer/Panel.get("custom_styles/panel").shadow_color.a = 0
+	VisualServer.canvas_item_set_z_index(self.get_canvas_item(), 9999)	# Not working
 
 func open():
 	self.show()
@@ -10,6 +11,10 @@ func open():
 	$Tween.interpolate_property($HBoxContainer/Panel, "custom_styles/panel:shadow_color:a", $HBoxContainer/Panel.get("custom_styles/panel").shadow_color.a, 1, 0.5, Tween.TRANS_EXPO, Tween.EASE_OUT)
 	$Tween.start()
 
+func close():
+	$Tween.interpolate_property(self, "rect_global_position", self.rect_global_position, Vector2( - rect_size.x, 0), 0.5, Tween.TRANS_EXPO, Tween.EASE_OUT)
+	$Tween.interpolate_property($HBoxContainer/Panel, "custom_styles/panel:shadow_color:a", $HBoxContainer/Panel.get("custom_styles/panel").shadow_color.a, 0, 0.5, Tween.TRANS_EXPO, Tween.EASE_OUT)
+	$Tween.start()
 
 func _on_SideMenuButton3_pressed():
 	pass # Replace with function body.
@@ -28,9 +33,7 @@ func _on_SideMenuButton2_pressed():
 
 func _on_Control_gui_input(event):
 	if Input.is_action_just_pressed("click"):
-		$Tween.interpolate_property(self, "rect_global_position", self.rect_global_position, Vector2( - rect_size.x, 0), 0.5, Tween.TRANS_EXPO, Tween.EASE_OUT)
-		$Tween.interpolate_property($HBoxContainer/Panel, "custom_styles/panel:shadow_color:a", $HBoxContainer/Panel.get("custom_styles/panel").shadow_color.a, 0, 0.5, Tween.TRANS_EXPO, Tween.EASE_OUT)
-		$Tween.start()
+		close()
 
 
 func _on_SideMenuButton5_pressed():
