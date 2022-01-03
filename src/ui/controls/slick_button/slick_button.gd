2718fca37354f980566a445996b74110da0382b5
SpyrexDE <malikfk100@gmail.com>
2022-01-03 19:01:11 +0100

    Fix slick_button

--- src/ui/controls/slick_button/slick_button.gd
+++ src/ui/controls/slick_button/slick_button.gd
@@ -3,15 +3,19 @@ extends NinePatchRect
 signal held
 signal pressed
 
+# Without cooldown _on_SlickButton_gui_input gets called twice, idk why
+var cooldown = 0
+
 func _on_SlickButton_gui_input(event):
 	if Input.is_action_just_pressed("click"):
 		$AnimationPlayer.play("down")
 		emit_signal("held")
 	elif Input.is_action_just_released("click"):
-		if !check_hover():
+		if !check_hover() || cooldown != 0:
 			return
 		$AnimationPlayer.play("up")
 		emit_signal("pressed")
+		cooldown = 100
 
 func check_hover():
 	var rect_check = Rect2(Vector2(0,0), self.get_global_rect().size)
@@ -23,6 +27,9 @@ func check_hover():
 	else:
 		return true
 
+func _physics_process(delta):
+	if cooldown > 0:
+		cooldown -= 1
 
 func _on_AnimationPlayer_animation_finished(anim_name):
 	$AnimationPlayer.playback_speed = 1
